f6896cdde5b81692ea843272ab2e99949e29561f
pc37ivag <pc37ivag@STKPOOLPC09>
2024-03-27 09:26:55 +0100

    Projektdateien hinzufügen.

---
+++ WPF 27.03.2024/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_27._03._2024
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Anmelden_Click(object sender, RoutedEventArgs e)
+        {
+           string text = VornameBox.Text + " " + NachnameBox.Text;
+           AnmeldenLabel.Content = text;
+        }
+
+        private void Löschen_Click(object sender, RoutedEventArgs e)
+        {
+            VornameBox.Text = "";
+            NachnameBox.Clear();
+            AnmeldenLabel.Content = "";
+        }
+    }
+}
